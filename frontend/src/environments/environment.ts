c63065f71d553ccc1f58faeb1549cc8a9657d168
divyankbhalla <divyankbhalla@Divyanks-MacBook-Air.local>
2026-07-26 15:51:57 +0530

    environmental changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:5001/api',
-    socketUrl: 'http://localhost:5001',
+    // apiUrl: 'http://localhost:5001/api', for local development
+    // socketUrl: 'http://localhost:5001',
+    apiUrl: 'https://eminence-innovation-assignment.onrender.com/api', // for production deployment
+    socketUrl: 'https://eminence-innovation-assignment.onrender.com',
 };
